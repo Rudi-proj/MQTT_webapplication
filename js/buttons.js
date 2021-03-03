ba243f89821dc77f510a6c2bfc089838f7731109
Florian Rudolf <rudolf@hs-ulm.de>
2021-03-03 12:40:41 +0100

    added javascript

---
+++ js/buttons.js
@@ -0,0 +1,14 @@
+mqtt_connect.onclick = function mqtt_connect()
+{
+    alert('MQTT_connect');
+}
+
+mqtt_subscribe.onclick = function mqtt_subscribe()
+{
+    alert('MQTT_subscribe');
+}
+
+mqtt_publish.onclick = function mqtt_publish()
+{
+    alert('MQTT_publish');
+}
